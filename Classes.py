f10227a31614eb95703e8ee6335fa17b5578031d
Vitor Santos <vitorsacz@gmail.com>
2024-06-06 23:51:17 -0300

    CRUD completo - v1

--- Classes.py
+++ Classes.py
@@ -1,5 +1,7 @@
 import oracledb
 import json
+import os
+import csv
 
 class Conexao:
 
@@ -11,7 +13,7 @@ class Conexao:
             USER = credenciais["user"]
             PASS = credenciais["pass"]
         
-            # Conecta o servidor
+            # Conecta ao servidor
             dsnStr = oracledb.makedsn("oracle.fiap.com.br", 1521, "ORCL")
             # Efetua a conexão com o Usuário
             connection = oracledb.connect(user=USER, password=PASS, dsn=dsnStr)
@@ -24,32 +26,24 @@ class Conexao:
             print(f"Conexão mal-sucedida: {e}")
 
 class Ong(Conexao):
-    def __init__(self, nome, pais, estado, area_atuacao):
-        self.nome = nome
-        self.pais = pais
-        self.estado = estado
-        self.area_atuacao = area_atuacao
 
-    @staticmethod
-    def menu_ong():
-        print("1- Inserir ONG")
-        print("2- Alterar dados da ONG")
-        print("3- Excluir ONG")
-        print("4- Listar ONG's")
-        print("5- SAIR")
+    def __init__(self):
+        pass
 
-    def inserir_ong(self, id_produto, nome, descricao, area_atuacao, valor):
+    
+
+    def inserir_ong(self, id_ong, nome, pais, estado, area_atuacao, material_coletado, imagem):
         connection = self.conexao_db()
 
         try:
             cursor = connection.cursor()
-            sql = "INSERT INTO T_GS_ONG (id_produto, nome, descricao, area_atuacao, valor) VALUES (:1, :2, :3, :4, :5)"
-            cursor.execute(sql, (id_produto, nome, descricao, area_atuacao, valor))
+            sql = "INSERT INTO T_GS_ONG (idOng, nome, pais, estado, areaAtuacao, materialColetado, imagem) VALUES (:1, :2, :3, :4, :5, :6, :7)"
+            cursor.execute(sql, (id_ong, nome, pais, estado, area_atuacao, material_coletado, imagem))
             connection.commit()
-            print("Produto inserido com sucesso!")
+            print("\nONG inserida com sucesso!")
 
         except oracledb.Error as error:
-            print(f"Erro ao inserir produto: {error}")
+            print(f"Erro ao inserir ONG: {error}")
 
         finally:
             if cursor:
@@ -65,8 +59,16 @@ class Ong(Conexao):
             cursor.execute("SELECT * FROM T_GS_ONG")
             ongs = cursor.fetchall()
 
+            print("Lista de Produtos:")
+            print("=================")
             for ong in ongs:
-                print(ong)
+                print(f"ID: {ong[0]}")
+                print(f"Nome: {ong[1]}")
+                print(f"Pais: {ong[2]}")
+                print(f"Estado: {ong[3]}")
+                print(f"Área de Atuação: {ong[4]}")
+                print(f"Material Coletado: {ong[5]} toneladas")
+                print("-----------------")
 
         except oracledb.Error as error:
             print(f"Erro ao listar ONGs: {error}")
@@ -77,15 +79,34 @@ class Ong(Conexao):
             if connection:
                 connection.close()
 
-    def alterar_ong(self, id_produto, nome, descricao, area_atuacao, valor):
+    def excluir_ong(self, id_ong):
         connection = self.conexao_db()
 
         try:
             cursor = connection.cursor()
-            sql = "UPDATE T_GS_ONG SET nome = :1, descricao = :2, area_atuacao = :3, valor = :4 WHERE id_produto = :5"
-            cursor.execute(sql, (nome, descricao, area_atuacao, valor, id_produto))
+            sql = "DELETE FROM T_GS_ONG WHERE idOng = :1"
+            cursor.execute(sql, (id_ong,))
             connection.commit()
-            print("ONG alterada com sucesso!")
+            print("\nONG excluída com sucesso!")
+
+        except oracledb.Error as error:
+            print(f"Erro ao excluir ONG: {error}")
+
+        finally:
+            if cursor:
+                cursor.close()
+            if connection:
+                connection.close()
+
+    def alterar_ong(self, idOng, novo_nome, novo_pais, novo_estado, nova_area_atuacao, novo_material_coletado, nova_imagem):
+        connection = self.conexao_db()
+
+        try:
+            cursor = connection.cursor()
+            sql = "UPDATE T_GS_ONG SET nome = :1, pais = :2, estado = :3, areaAtuacao = :4, materialColetado = :5, imagem = :6 WHERE idOng = :7"
+            cursor.execute(sql, (novo_nome, novo_pais, novo_estado, nova_area_atuacao, novo_material_coletado, nova_imagem, idOng))
+            connection.commit()
+            print("\nONG alterada com sucesso!")
 
         except oracledb.Error as error:
             print(f"Erro ao alterar ONG: {error}")
@@ -96,3 +117,119 @@ class Ong(Conexao):
             if connection:
                 connection.close()
 
+class DatabaseCsv(Conexao):
+
+    def __init__(self):
+        pass
+
+    def insert_qualidade_ar_agua(self, filepath):
+        connection = self.conexao_db()
+        try:
+            with open(filepath, 'r', encoding='utf-8') as file:
+                reader = csv.reader(file)
+                next(reader)  # Pula o cabeçalho
+
+                cursor = connection.cursor()
+                id_counter = 1  # Inicia o contador de ID
+                
+                for row in reader:
+                    cidade, regiao, entidade, qualidade_do_ar, poluicao_agua = row
+                    cursor.execute('''
+                        INSERT INTO T_GS_QUALIDADE_AR_AGUA_CIDADE (id_cidade, cidade, regiao, entidade, qualidade_do_ar, poluicao_agua)
+                        VALUES (:1, :2, :3, :4, :5, :6)
+                    ''', (str(id_counter), cidade, regiao, entidade, qualidade_do_ar, poluicao_agua))
+                    id_counter += 1  # Incrementa o contador de ID
+                connection.commit()
+                print("Dados inseridos com sucesso na tabela T_GS_QUALIDADE_AR_AGUA_CIDADE.")
+
+        except Exception as e:
+            print(f"Erro ao inserir dados: {e}")
+
+        finally:
+            if cursor:
+                cursor.close()
+            if connection:
+                connection.close()
+
+    def insert_plastico_produzido(self, filepath):
+        connection = self.conexao_db()
+        try:
+            with open(filepath, 'r', encoding='utf-8') as file:
+                reader = csv.reader(file)
+                next(reader)  # Pula o cabeçalho
+
+                cursor = connection.cursor()
+                id_counter = 1  # Inicia o contador de ID
+                for row in reader:
+                    entidade, codigo, ano, producao_anual = row
+                    cursor.execute('''
+                        INSERT INTO T_GS_PLASTICO_PRODUZIDO (id_ano, ano, producao_anual)
+                        VALUES (:1, :2, :3)
+                    ''', (str(id_counter), int(ano), float(producao_anual)))
+                    id_counter += 1  # Incrementa o contador de ID
+                connection.commit()
+                print("Dados inseridos com sucesso na tabela T_GS_PLASTICO_PRODUZIDO.")
+
+        except Exception as e:
+            print(f"Erro ao inserir dados: {e}")
+
+        finally:
+            if cursor:
+                cursor.close()
+            if connection:
+                connection.close()
+
+class Design:
+
+    def __init__(self):
+        pass
+
+    def limpa_tela(self):
+        # Verifica o sistema operacional
+        if os.name == 'posix':  # Para Linux e macOS
+            _ = os.system('clear')
+        elif os.name == 'nt':  # Para Windows
+            _ = os.system('cls')
+        else:
+            # Se não estiver em nenhum dos sistemas acima, limpa usando caracteres de nova linha
+            print('\n' * 100)  # Imprime 100 novas linhas para "limpar" a tela
+
+    def cabecalho(self):
+        self.limpa_tela()
+        print("")
+        print("""     _         _       _           _ 
+                     | |       | |     | |         | |
+   ___ _ __ _   _  __| |   __ _| | ___ | |__   __ _| |
+  / __| '__| | | |/ _` |  / _` | |/ _ \| '_ \ / _` | |
+ | (__| |  | |_| | (_| | | (_| | | (_) | |_) | (_| | |
+  \___|_|   \__,_|\__,_|  \__, |_|\___/|_.__/ \__,_|_|
+                           __/ |                      
+                          |___/ """)
+
+    def saida(self):
+        self.limpa_tela()
+        print(""" 
+        _          _                 _       
+       | |        (_)               | |      
+   ___ | |__  _ __ _  __ _  __ _  __| | ___  
+  / _ \| '_ \| '__| |/ _` |/ _` |/ _` |/ _ \ 
+ | (_) | |_) | |  | | (_| | (_| | (_| | (_) |
+  \___/|_.__/|_|  |_|\__, |\__,_|\__,_|\___/ 
+                      __/ |                  
+                     |___/ """)
+        
+
+    @staticmethod
+    def menu():
+        print("1- Inserir ONG")
+        print("2- Listar ONG's")
+        print("3- Excluir ONG")
+        print("4- Alterar dados da ONG")
+        print("5- Inserir Csv")
+        print("6- SAIR")
+
+# Exemplo de uso
+if __name__ == "__main__":
+    db_csv = DatabaseCsv()
+    db_csv.insert_qualidade_ar_agua('./data/qualidade_ar_agua.csv')
+    db_csv.insert_plastico_produzido('./data/plastico_produzido.csv')
--- main.py
+++ main.py
@@ -1,13 +1,118 @@
+from Classes import Ong, Design, DatabaseCsv
+import time
+
+o = Ong()
+d = Design()
+data = DatabaseCsv()
+
+
+
 def main():
     while True:
+        d.cabecalho()
+        d.menu()
 
+        op = int(input("\nInforme a opção escolhida: "))
+
+        if op == 1:
+
+            while True:
+            
+                id_ong = input("Informe o id da ONG: ")
+                nome = input("Informe o nome da ONG: ")
+                pais = input("Informe o pais: ")
+                estado = input("Informe o estado: ")
+                area_atuacao = input("Informe a area de atuação: ")
+                material_coletado = input("Informe a quantidade de material coletado: ")
+                imagem = input("\nInforme o nome da imagem (com a extenção .png): ")
+
+                o.inserir_ong(id_ong, nome, pais, estado, area_atuacao, material_coletado, imagem)
+
+
+                continuar = int(input("\nGostaria de inserir uma nova ONG?\n\n1- SIM\n2-NÃO\n"))
+
+                if continuar == 2:
+                    break
+
+            
+
+        elif op == 2:
+            o.listar_ongs()
+            time.sleep(10)
+            
+
+        elif op == 3:
+            while True:
+                d.cabecalho()
+                o.listar_ongs()
+
+                id_produto = int(input("Informe o id da ONG que deseja deletar: \n"))
+
+                op = int(input("Você tem certeza que deseja concluir a ação?\n1- SIM\n2- NÃO\n"))
+
+                if op == 1:
+                    o.excluir_ong(id_produto)
+                else:
+                    print("Continuando...")
+                    break
+
+                opcao = int(input("\n\nDeseja excluir outro produto? \n1- SIM\n2- NÃO\n"))
+
+                if opcao == 2:
+                    break
+
+        elif op == 4:
+            while True:
+                d.cabecalho()
+                o.listar_ongs()
+                
+                id_ong = int(input("\n\nInforme o id do produto que você deseja alterar: \n"))
+
+
+                novo_nome = input('Informe o novo nome da ONG: ')
+                novo_pais = input('Informe o novo pais da ONG: ')
+                novo_estado = input('Informe o novo estado da ONG: ')
+                nova_area_atuacao = input('Informe a área de atuação da ONG: ')
+                novo_material_coletado = int(input('Informe o valor do lixo coletado: '))
+                
+                nova_imagem = input('Informe o nome da imagem: ')
+
+                o.alterar_ong(id_ong, novo_nome, novo_pais, novo_estado, nova_area_atuacao, novo_material_coletado, nova_imagem)
+
+
+                continuar = int(input("\nGostaria de alterar outra ONG?\n\n1- SIM\n2-NÃO\n"))
+
+                if continuar == 2:
+                    break
         
+        elif op == 5:
+             while True:
+                print("\n1- Csv de qualidade do ar e poluição da água")
+                print("2- Csv de produção de plástico per-capita\n\n")
 
-        pass
+                opcao_csv = int(input("Informe sua opção: "))
+                if opcao_csv == 1:
+                    print("Inserindo dados...")
+                    data.insert_qualidade_ar_agua("./data/5- poluicao-agua-cidades.csv")
+                    time.sleep(5)
 
+                    break
 
+                elif opcao_csv == 2:
+                    print("Inserindo dados...")
+                    data.insert_plastico_produzido("./data/4- desperdicio-plastico-per-capita.csv")
+                    time.sleep(5)
+                    break
 
+                else:
+                    print("Opção inválida!")
+                    time.sleep(2)
+                    d.limpa_tela()
+                    d.cabecalho()
 
+        elif op == 6:
+            d.saida()
+            break
 
 
 if __name__ == "__main__":
